9fdd500a109dde107d572f3d8dcfc6f7ca23563e
gig-jagg <gig.jaggernauth@gmail.com>
2025-07-02 07:58:11 -0400

    remove double code

--- profile-script.js
+++ profile-script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const boxData = [
         
     { title: "LinkedIn",
@@ -22,28 +21,3 @@ boxData.forEach(item => {
 
         container.appendChild(profileBox); 
     });
-=======
-const boxData = [
-        
-    { title: "LinkedIn",
-    link:"https://www.linkedin.com/in/gregory-jaggernauth/",
-    image:"assets/linkedin.png"},
-
-    { title: "GitHub", 
-    link:"https://github.com/gig-jagg",
-    image:"assets/github.png"},
-    ];
-
-const container = document.getElementById("profile-container"); 
-
-boxData.forEach(item => {
-    const profileBox = document.createElement("div"); 
-    profileBox.classList.add("profile-box");
-
-    profileBox.innerHTML = `
-        <img src=${item.image} alt=${item.title} width="20" height="20">
-        <a class="links" href=${item.link} target="_blank" rel="noopener noreferrer">${item.title}</a>`
-
-        container.appendChild(profileBox); 
-    });
->>>>>>> 7a50195c8f5003e1eb5f01ddf94f324fead87794
